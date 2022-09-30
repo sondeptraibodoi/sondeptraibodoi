1551fecec5445c59b5d0fdab41938acd96d8f780
sondeptraibodoi <91931248+sondeptraibodoi@users.noreply.github.com>
2022-09-30 21:06:18 +0700

    Add files via upload

---
+++ js/filter.js
@@ -0,0 +1,583 @@
+
+var list = [
+    { id: 1,
+       image: "image/7-vien-ngoc-rong.jpg", 
+       title: "Dragon Ball",
+       "Link": "dragonball.html",
+       theloai:"Action"
+       },
+    { id: 2,
+       image: "image/300.jpg",
+        title: "300" ,
+        "Link": "300.html",
+        theloai:"Adventure"
+      },
+    { id: 3,
+       image: "image/ansatsu-kyoushitsu.jpg",
+        title: "Ansatsu Kyou" ,
+        "Link": "ansatsu-kyoushitsu.html",
+        theloai:"Fantasy"
+      },
+    { id: 4,
+       image: "image/aquaman.jpg",
+        title: "Aquaman" ,
+        "Link": "aquaman.html",
+        theloai:"Comic"
+      },
+    { id: 5,
+       image: "image/attack-on-titan.jpg", 
+       title: "Attack on Titan",
+       "Link": "attack-on-titan.html",
+       theloai:"Action"
+       },
+    { id: 6,
+       image: "image/baki-son-of-ogre.jpg", 
+       title: "Baki" ,
+       "Link": "baki-son-of-ogre.html",
+       theloai:"Action"
+      },
+    { id: 7,
+       image: "image/batman-detective.jpg", 
+       title: "Batman detective" ,
+       "Link": "batman-detective.html",
+       theloai:"Comic"
+      },
+    { id: 8,
+       image: "image/batman-white-knight.jpg", 
+       title: "Batman white",
+       "Link": "batman-white-knight.html",
+       theloai:"Comic"
+       },
+    { id: 9,
+       image: "image/berserk.jpg", 
+       title: "Berserk" ,
+       "Link": "berserk.html",
+       theloai:"Action"
+      },
+    { id: 10,
+       image: "image/bien-nien-su-han-thu-chronicles-of-hate.jpg", 
+       title: "Chronicles of hate" ,
+       "Link": "bien-nien-su-han-thu-chronicles-of-hate.html",
+       theloai:"Adventure"
+      },
+    { id: 11,
+       image: "image/black-clover.jpg", 
+       title: "Black Clover",
+       "Link": "black-clover.html",
+       theloai:"Fantasy"
+       },
+    { id: 12,
+       image: "image/bleach.jpg", 
+       title: "Bleach" ,
+       "Link": "bleach.html",
+       theloai:"Fantasy"
+      },
+    { id: 13,
+       image: "image/blue-lock.jpg", 
+       title: "Blue Lock",
+       "Link": "blue-lock.html",
+       theloai:"Sport"
+       },
+    { id: 14,
+       image: "image/blue-period.jpg", 
+       title: "Blue period",
+       "Link": "blue-period.html" ,
+       theloai:"Manga"
+      },
+    { id: 15,
+       image: "image/boku-no-hero-academia.jpg", 
+       title: "Boku no hero",
+       "Link": "boku-no-hero-academia.html",
+       theloai:"Manga"
+       },
+    { id: 16,
+       image: "image/conan.jpg", 
+       title: "Conan" ,
+       "Link": "conan.html",
+       theloai:"Trinh Thám"
+      },
+    { id: 17,
+       image: "image/cuoc-phieu-luu-bi-an.jpg", 
+       title: "Jojo's" ,
+       "Link": "cuoc-phieu-luu-bi-an.html",
+       theloai:"Adventure"
+      },
+    { id: 18,
+       image: "image/cyborg.jpg", 
+       title: "Cyborg" ,
+       "Link": "cyborg.html",
+       theloai:"Comic"
+      },
+    { id: 19,
+       image: "image/chainsaw-man.jpg", 
+       title: "Chainsaw man" ,
+       "Link": "chainsaw-man.html",
+       theloai:"Action"
+      },
+    { id: 20,
+       image: "image/dark-nights-metal.jpg", 
+       title: "Dark night metal" ,
+       "Link": "dark-nights-metal.html",
+       theloai:"Comic"
+      },
+    { id: 21,
+       image: "image/deadpool.jpg", 
+       title: "Deadpool" ,
+       "Link": "deadpool.html",
+       theloai:"Comic"
+      },
+    { id: 22,
+       image: "image/death-note.jpg", 
+       title: "Death Note" ,
+       "Link": "death-note.html",
+       theloai:"Trinh Thám"
+      },
+    { id: 23,
+       image: "image/doraemon.jpg", 
+       title: "Doraemon" ,
+       "Link": "doraemon.html",
+       theloai:"Drama"
+      },
+    { id: 24,
+       image: "image/doraemon-plus.jpg", 
+       title: "Doraemon plus" ,
+       "Link": "doraemon-plus.html",
+       theloai:"Drama"
+      },
+    { id: 25,
+       image: "image/dr-stone.jpg", 
+       title: "Dr,Stone" ,
+       "Link": "dr-stone.html",
+       theloai:"Adventure"
+      },
+    { id: 26,
+       image: "image/fairy-tail-100-year-quest.jpg", 
+       title: "Fairy Tail" ,
+       "Link": "fairy-tail-100-year-quest.html",
+       theloai:"Drama"
+      },
+    { id: 27, 
+      image: "image/gantz.jpg", 
+      title: "Gantz" ,
+      "Link": "gantz.html",
+      theloai:"Drama"
+    },
+    { id: 28,
+       image: "image/gintama.jpg", 
+       title: "Gintama" ,
+       "Link": "gintama.html",
+       theloai:"Martial Arts"
+      },
+    {
+        "image": "image/hiep-si-arkham-khoi-nguyen.jpg",
+        "title": "Batman:Arkham",
+        "Link": "hiep-si-arkham-khoi-nguyen.html",
+        "id": "29",
+        theloai:"Mystery"
+       },
+       {
+        "image": "image/higanjima.jpg",
+        "title": "Higanjima",
+        "Link": "higanjima.html",
+        "id": "30",
+        theloai:"Horror"
+       },
+       {
+        "image": "image/hunter-x-hunter.jpg",
+        "title": "Hunter x Hunter",
+        "Link": "hunter-x-hunter.html",
+        "id": "31",
+        theloai:"Drama"
+       },
+       {
+        "image": "image/injustice-gods-among-us.jpg",
+        "title": "Injustice I",
+        "Link": "injustice-gods-among-us.html",
+        "id": "32",
+        theloai:"Fantasy"
+       },
+       {
+        "image": "image/john-wick.jpg",
+        "title": "John Wick",
+        "Link": "john-wick.html",
+        "id": "33",
+        theloai:"Action"
+       },
+       {
+        "image": "image/jujutsu-kaisen.jpg",
+        "title": "Jujutsu Kaisen",
+        "Link": "jujutsu-kaisen.html",
+        "id": "34",
+        theloai:"School Life"
+       },
+       {
+        "image": "image/justice-league.jpg",
+        "title": "Justice League",
+        "Link": "justice-league.html",
+        "id": "35",
+        theloai:"Martial Arts"
+       },
+       {
+        "image": "image/kanojo-okarishimasu.jpg",
+        "title": "Kanojo Okarishimasu",
+        "Link": "kanojo-okarishimasu.html",
+        "id": "36",
+        theloai:"Horror"
+       },
+       {
+        "image": "image/kengan-ashua.jpg",
+        "title": "Kengan Ashua",
+        "Link": "kengan-ashua.html",
+        "id": "37",
+        theloai:"Sport"
+       },
+       {
+        "image": "image/kengan-omega.jpg",
+        "title": "Kengan Omega",
+        "Link": "kengan-omega.html",
+        "id": "38",
+        theloai:"Sport"
+       },
+       {
+        "image": "image/kimetsu-no-yaiba.jpg",
+        "title": "Kimetsu No Yaiba",
+        "Link": "kimetsu-no-yaiba.html",
+        "id": "39",
+        theloai:"Isekai"
+       },
+       {
+        "image": "image/kingdom-vuong-gia-thien-ha.jpg",
+        "title": "Kristin Lehner",
+        "Link": "kingdom-vuong-gia-thien-ha.html",
+        "id": "40",
+        theloai:"Seinen"
+       },
+       {
+        "image": "image/lang-khach.jpg",
+        "title": "Lãng Khách",
+        "Link": "lang-khach.html",
+        "id": "42",
+        theloai:"MaTure"
+       },
+       {
+        "image": "image/doraemon-bong-chay.jpg",
+        "title": "Doraemon Bóng Chày",
+        "Link": "doraemon-bong-chay.html",
+        "id": "43",
+        theloai:"Fantasy"
+       },
+       {
+        "image": "image/mob-psycho-100.jpg",
+        "title": "Mob Psycho 100",
+        "Link": "mob-psycho-100.html",
+        "id": "44",
+        theloai:"Psychological"
+       },
+       {
+        "image": "image/nanatsu-no-taizai.jpg",
+        "title": "nanatsu-no-taizai",
+        "Link": "nanatsu-no-taizai.html",
+        "id": "45",
+        theloai:"Mystery"
+       },
+       {
+        "image": "image/moon-knight.jpg",
+        "title": "moon-knight",
+        "Link": "moon-knight.html",
+        "id": "46",
+        theloai:"Psychological"
+       },
+       {
+        "image": "image/naruto.jpg",
+        "title": "naruto",
+        "Link": "naruto.html",
+        "id": "47",
+        theloai:"Shoujo"
+       },
+       {
+        "image": "image/naruto-full-color-edition.jpg",
+        "title": "naruto-full-color-edition",
+        "Link": "naruto-full-color-edition.html",
+        "id": "48",
+        theloai:"Manga"
+       },
+       {
+        "image": "image/nguoi-doi-tinh-anh-quy-tu.jpg",
+        "title": "nguoi-doi-tinh-anh-quy-tu",
+        "Link": "nguoi-doi-tinh-anh-quy-tu.html",
+        "id": "49",
+        theloai:"Comic"
+       },
+       {
+        "image": "image/onepunch-man.jpg",
+        "title": "onepunch-man",
+        "Link": "onepunch-man.html",
+        "id": "50",
+        theloai:"One Shot"
+       },
+       {
+        "image": "image/planet-hulk.jpg",
+        "title": "planet-hulk",
+        "Link": "planet-hulk.html",
+        "id": "51",
+        theloai:"Comic"
+       },
+       {
+        "image": "image/rurouni-kenshin_1552462006.jpg",
+        "title": "rurouni-kenshin",
+        "Link": "rurouni-kenshin_1552462006.html",
+        "id": "52",
+        theloai:"School Life"
+       },
+       {
+        "image": "image/spy-x-family.jpg",
+        "title": "spy-x-family",
+        "Link": "spy-x-family.html",
+        "id": "54",
+        theloai:"MaTure"
+       },
+       {
+        "image": "image/sweet-home.jpg",
+        "title": "sweet-home",
+        "Link": "sweet-home.html",
+        "id": "55",
+        theloai:"Manhua"
+       },
+       {
+        "image": "image/tay-du.jpg",
+        "title": "tay-du",
+        "Link": "tay-du.html",
+        "id": "56",
+        theloai:"Martial Arts"
+       },
+       {
+        "image": "image/tieu-nhan.jpg",
+        "title": "tieu-nhan",
+        "Link": "tieu-nhan.html",
+        "id": "57",
+        theloai:"Webtoon"
+       },
+       {
+        "image": "image/tokyo-ghoul.jpg",
+        "title": "tokyo-ghoul",
+        "Link": "tokyo-ghoul.html",
+        "id": "58",
+        theloai:"josei"
+       },
+       {
+        "image": "image/tokyo-ghoulre.jpg",
+        "title": "tokyo-ghoulre",
+        "Link": "tokyo-ghoulre.html",
+        "id": "59",
+        theloai:"Drama"
+       },
+       {
+        "image": "image/tokyorevengers.jpg",
+        "title": "tokyorevengers",
+        "Link": "tokyorevengers.html",
+        "id": "60",
+        theloai:"Shounen"
+       },
+       {
+        "image": "image/toriko.jpg",
+        "title": "toriko",
+        "Link": "toriko.html",
+        "id": "61",
+        theloai:"Webtoon"
+       },
+       {
+        "image": "image/tougen-anki.jpg",
+        "title": "tougen-anki",
+        "Link": "tougen-anki.html",
+        "id": "62",
+        theloai:"josei"
+      },
+      {
+        "image": "image/the-flash.jpg",
+        "title": "the-flash",
+        "Link": "the-flash.html",
+        "id": "63",
+        theloai:"Shounen"
+      },
+      {
+        "image": "image/the-god-of-high-school.jpg",
+        "title": "the-god-of-high-school",
+        "Link": "the-god-of-high-school.html",
+        "id": "64",
+        theloai:"Isekai"
+      },
+      {
+        "image": "image/the-legend-of-wonder-woman.jpg",
+        "title": "the-legend-of-wonder-woman",
+        "Link": "the-legend-of-wonder-woman.html",
+        "id": "65",
+        theloai:"Mecha"
+      },
+      {
+        "image": "image/the-promised-neverland.jpg",
+        "title": "the-promised-neverland",
+        "Link": "the-promised-neverland.html",
+        "id": "66",
+        theloai:"Mecha"
+      },
+      {
+        "image": "image/the-walking-dead.jpg",
+        "title": "the-walking-dead",
+        "Link": "the-walking-dead.html",
+        "id": "67",
+        theloai:"Adult"
+      },
+      {
+        "image": "image/thor-2015.jpg",
+        "title": "thor-2015",
+        "Link": "thor-2015.html",
+        "id": "68",
+        theloai:"Seinen"
+      },
+      {
+        "image": "image/uzumaki-boruto.jpg",
+        "title": "uzumaki-boruto",
+        "Link": "uzumaki-boruto.html",
+        "id": "69",
+        theloai:"Shounen"
+      },
+      {
+        "image": "image/wind-breaker.jpg",
+        "title": "wind-breaker",
+        "Link": "wind-breaker.html",
+        "id": "70",
+        theloai:"Sport"
+      },
+      {
+        "image": "image/yugi-oh-full-color-edition.jpg",
+        "title": "yugi-oh-full-color-edition",
+        "Link": "yugi-oh-full-color-edition.html",
+        "id": "71",
+        theloai:"Shoujo"
+      }
+  ]
+  
+  let perPage = 30;
+  let currentPage = 1;
+  let start = 0;
+  let end = perPage;
+  
+  const totalPages = Math.ceil(list.length/perPage);
+  console.log(totalPages)
+  
+  const btnNext = document.querySelector('.btn-next');
+  const btnPrev = document.querySelector('.btn-prev');
+  
+  function getCurrentPage(currentPage){
+    start = (currentPage - 1) * perPage;
+    end = currentPage * perPage;
+  }
+  
+  function renderProduct(theloaichon_arr=[]) {
+    var product =document.getElementById('product')
+      product.innerHTML='';
+      for(i=0; i<list.length;i++){
+        theloaiTruyen=list[i].theloai;
+        image=list[i].image;
+        ten=list[i].title;
+        link=list[i].Link
+      if(theloaichon_arr.length>0){
+        if(theloaichon_arr.includes(list[i].theloai)==false)continue;
+      }
+      const content=list.map((item,index)=>{
+        if(index >= start && index < end){
+          
+        }
+      })
+      product.innerHTML+=`
+        <div class="image-box">
+        <a href="${list[i].Link}">
+        <img src="${list[i].image}">
+        </a>
+        <a href="${list[i].Link}">
+        <span>${list[i].title}</span>
+        </a>
+        </div>
+         `;
+    }
+      
+  }
+  renderProduct();
+  
+  function renderListPage(){
+    let html ='';
+    html +=`<li class="active"><a>${1}</a></li>`;
+    for(let i=2; i<= totalPages;i++){
+      html += `<li><a>${i}</a></li>`
+    }
+    document.getElementById('number-page').innerHTML =html;
+  }
+  renderListPage();
+  
+  function changPage(){
+    const currentPages = document.querySelectorAll('.number-page li');
+    console.log(currentPage);
+    for(let i=0; i< currentPages.length; i++) {
+      currentPages[i].addEventListener('click',() =>{
+        let value = i + 1;
+        console.log(value);
+        currentPage = value;
+        $('.number-page li').removeClass('active');
+        currentPages[i].classList.add('active');
+        if(currentPage >1 && currentPage < totalPages){
+          $('.btn-prev').removeClass('btn-active');
+          $('.btn-next').removeClass('btn-active');
+        }
+        if(currentPage===1){
+          $('.btn-prev').addClass('btn-active');
+        }
+        if(currentPage===totalPages){
+          $('.btn-next').addClass('btn-active');
+        }
+        getCurrentPage(currentPage);
+        renderProduct();
+      })
+    }
+  }
+  changPage();
+  
+  btnNext.addEventListener('click',()=>{
+    currentPage++;
+    if(currentPage > totalPages){
+      currentPage = totalPages;
+    }
+    if(currentPage === totalPages){
+      $('.btn-next').addClass('btn-active');
+    }
+    $('.btn-prev').removeClass('btn-active');
+    $('.number-page li').removeClass('active');
+    $(`.number-page li:eq(${currentPage-1})`).addClass('active');
+    getCurrentPage(currentPage);
+    renderProduct();
+  })
+  
+  btnPrev.addEventListener('click',()=>{
+    currentPage--;
+    if(currentPage < 1){
+      currentPage = 1;
+    }
+    if(currentPage===1){
+      $('.btn-prev').addClass('btn-active');
+    }
+    $('.btn-next').removeClass('btn-active');
+    $('.number-page li').removeClass('active');
+    $(`.number-page li:eq(${currentPage-1})`).addClass('active');
+    getCurrentPage(currentPage);
+    renderProduct();
+  })
+  
+  ///loc
+  
+  function chonTL(){
+    var arr1 = document.getElementsByClassName('theloai');
+    var theloaichon_arr=[];
+    for(i=0; i<arr1.length;i++){
+      if(arr1[i].checked==true) theloaichon_arr.push(arr1[i].value);
+    }
+  renderProduct(theloaichon_arr);
+  }
